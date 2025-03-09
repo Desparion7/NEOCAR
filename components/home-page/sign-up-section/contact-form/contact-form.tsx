4c0119253a69e5b2b962da5fbaf47f410ba81d35
wosmateusz3@gmail.com <wosmateusz3@gmail.com>
2025-03-09 10:47:04 +0100

    add category

--- components/home-page/sign-up-section/contact-form/contact-form.tsx
+++ components/home-page/sign-up-section/contact-form/contact-form.tsx
@@ -2,12 +2,12 @@
 
 import React from 'react';
 import {
-  useForm,
-  SubmitHandler,
-  Controller,
-  ControllerRenderProps,
-  FieldValues,
-  Path,
+	useForm,
+	SubmitHandler,
+	Controller,
+	ControllerRenderProps,
+	FieldValues,
+	Path,
 } from 'react-hook-form';
 import { sendFormData, FormData } from '@/lib/contact-form';
 import { motion } from 'framer-motion';
@@ -15,98 +15,136 @@ import Link from 'next/link';
 import toast from 'react-hot-toast';
 
 const ContactForm = () => {
-  const { control, handleSubmit, reset } = useForm<FormData>();
+	const { control, handleSubmit, reset } = useForm<FormData>();
 
-  const onSubmit: SubmitHandler<FormData> = async (data) => {
-    const url =
-      'https://osk-neocar.pl/wp/wp-json/contact-form-7/v1/contact-forms/6/feedback';
+	const onSubmit: SubmitHandler<FormData> = async (data) => {
+		const url =
+			'https://osk-neocar.pl/wp/wp-json/contact-form-7/v1/contact-forms/6/feedback';
 
-    try {
-      const result = await sendFormData(data, url);
-      if (result) {
-        toast.success('Twoje dane zostały przesłane.');
-      }
-      reset();
-    } catch (error) {
-      toast.error('Coś poszło nie tak, spróbuj ponownie później.');
-    }
-  };
+		try {
+			const result = await sendFormData(data, url);
+			if (result) {
+				toast.success('Twoje dane zostały przesłane.');
+			}
+			reset();
+		} catch (error) {
+			toast.error('Coś poszło nie tak, spróbuj ponownie później.');
+		}
+	};
 
-  return (
-    <form
-      onSubmit={handleSubmit(onSubmit)}
-      className=' flex flex-col mt-2 gap-3 justify-center w-full max-w-[30rem] '>
-      <Controller
-        name='yourName'
-        control={control}
-        defaultValue=''
-        rules={{ required: true }}
-        render={({ field }) => (
-          <Input field={field} type='text' placeholder='Imię i nazwisko' />
-        )}
-      />
-      <Controller
-        name='yourEmail'
-        control={control}
-        defaultValue=''
-        render={({ field }) => (
-          <Input field={field} type='email' placeholder='Twój e-mail' />
-        )}
-      />
-      <Controller
-        name='yourPhone'
-        control={control}
-        defaultValue=''
-        render={({ field }) => (
-          <Input
-            field={field}
-            type='number'
-            placeholder='Twoj numer telefonu'
-          />
-        )}
-      />
+	return (
+		<form
+			onSubmit={handleSubmit(onSubmit)}
+			className=' flex flex-col mt-2 gap-3 justify-center w-full max-w-[30rem] '
+		>
+			<Controller
+				name='yourName'
+				control={control}
+				defaultValue=''
+				rules={{ required: true }}
+				render={({ field }) => (
+					<Input
+						field={field}
+						type='text'
+						placeholder='Imię i nazwisko'
+					/>
+				)}
+			/>
+			<Controller
+				name='yourEmail'
+				control={control}
+				defaultValue=''
+				render={({ field }) => (
+					<Input
+						field={field}
+						type='email'
+						placeholder='Twój e-mail'
+					/>
+				)}
+			/>
+			<Controller
+				name='yourPhone'
+				control={control}
+				defaultValue=''
+				render={({ field }) => (
+					<Input
+						field={field}
+						type='number'
+						placeholder='Twoj numer telefonu'
+					/>
+				)}
+			/>
+			<Controller
+				name='category'
+				control={control}
+				defaultValue=''
+				rules={{ required: 'Wybierz kategorię prawa jazdy' }}
+				render={({ field }) => <SelectInput field={field} />}
+			/>
 
-      <p className='text-xs text-center lg:text-left'>
-        Zapisując się akceptujesz{' '}
-        <Link
-          href='/polityka-prywatnosci'
-          className='text-[var(--main-page-color)] font-semibold'>
-          politykę prywatności.
-        </Link>
-      </p>
-      <div className='flex justify-center mt-2 '>
-        <motion.button
-          whileHover={{ scale: 1.05 }}
-          whileTap={{ scale: 0.95 }}
-          transition={{ type: 'spring', stiffness: 300 }}
-          type='submit'
-          className='bg-[var(--main-page-color)] text-[var(--text-white-1)] px-2 py-3 rounded-md font-bold text-xs lg:text-base'>
-          Zapisz się na kurs
-        </motion.button>
-      </div>
-    </form>
-  );
+			<p className='text-xs text-center lg:text-left'>
+				Zapisując się akceptujesz{' '}
+				<Link
+					href='/polityka-prywatnosci'
+					className='text-[var(--main-page-color)] font-semibold'
+				>
+					politykę prywatności.
+				</Link>
+			</p>
+			<div className='flex justify-center mt-2 '>
+				<motion.button
+					whileHover={{ scale: 1.05 }}
+					whileTap={{ scale: 0.95 }}
+					transition={{ type: 'spring', stiffness: 300 }}
+					type='submit'
+					className='bg-[var(--main-page-color)] text-[var(--text-white-1)] px-2 py-3 rounded-md font-bold text-xs lg:text-base'
+				>
+					Zapisz się na kurs
+				</motion.button>
+			</div>
+		</form>
+	);
 };
 
 export default ContactForm;
 
 type InputProps<T extends FieldValues, K extends Path<T>> = {
-  field: ControllerRenderProps<T, K>;
-  type: string;
-  placeholder: string;
+	field: ControllerRenderProps<T, K>;
+	type: string;
+	placeholder: string;
 };
 
 const Input = <T extends FieldValues, K extends Path<T>>({
-  field,
-  type,
-  placeholder,
+	field,
+	type,
+	placeholder,
 }: InputProps<T, K>) => {
-  return (
-    <input
-      {...field}
-      type={type}
-      className='px-2 py-2 shadow-2xl'
-      placeholder={placeholder}
-    />
-  );
+	return (
+		<input
+			{...field}
+			type={type}
+			className='px-2 py-2 shadow-2xl'
+			placeholder={placeholder}
+		/>
+	);
+};
+const SelectInput = ({
+	field,
+}: {
+	field: ControllerRenderProps<FormData, 'category'>;
+}) => {
+	return (
+		<select {...field} className='px-2 py-2 shadow-2xl'>
+			<option value='' disabled>
+				Wybierz kategorię prawa jazdy
+			</option>
+			<option value='A'>A</option>
+			<option value='A1'>A1</option>
+			<option value='A2'>A2</option>
+			<option value='B'>B</option>
+			<option value='A+B'>A+B</option>
+			<option value='A1+B'>A1+B</option>
+			<option value='A2+B'>A2+B</option>
+		</select>
+	);
 };
--- lib/contact-form.ts
+++ lib/contact-form.ts
@@ -1,27 +1,28 @@
 export type FormData = {
-  yourName: string;
-  yourPhone: string;
-  yourEmail: string;
+	yourName: string;
+	yourPhone: string;
+	yourEmail: string;
+	category: string;
 };
 
 export const sendFormData = async (data: FormData, url: string) => {
-  let formBody = new FormData();
-  for (const [key, value] of Object.entries(data)) {
-    formBody.append(key, value.toString());
-  }
-  formBody.append('_wpcf7_unit_tag', '_wpcf7_unit_tag');
-  try {
-    const response = await fetch(url, {
-      method: 'POST',
-      body: formBody,
-    });
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-    const result = await response.json();
-    return result;
-  } catch (error) {
-    console.error('There was a problem with the fetch operation:', error);
-    throw error;
-  }
+	let formBody = new FormData();
+	for (const [key, value] of Object.entries(data)) {
+		formBody.append(key, value.toString());
+	}
+	formBody.append('_wpcf7_unit_tag', '_wpcf7_unit_tag');
+	try {
+		const response = await fetch(url, {
+			method: 'POST',
+			body: formBody,
+		});
+		if (!response.ok) {
+			throw new Error('Network response was not ok');
+		}
+		const result = await response.json();
+		return result;
+	} catch (error) {
+		console.error('There was a problem with the fetch operation:', error);
+		throw error;
+	}
 };
